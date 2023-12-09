d59f1d0a312ddc886fe72d1cd35a6bdbcae98f97
setal1te <149357036+cosholyknight@users.noreply.github.com>
2023-12-09 17:38:51 +0700

    Create Prob682.cpp
    done

---
+++ Prob682.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> score;
+        for (int i = 0; i < operations.size(); i++) {
+            if (operations[i] == "D") score.push_back(2 * score[score.size() - 1]);
+            else if (operations[i] == "+") score.push_back(score[score.size() - 2] + score[score.size() - 1]);
+            else if (operations[i] == "C") score.pop_back();
+            else score.push_back(atoi(operations[i].c_str()));
+        }
+        return accumulate(score.begin(), score.end(), 0);
+    }
+};
