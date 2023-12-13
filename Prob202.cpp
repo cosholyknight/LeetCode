eac1300af5968287e928b8b346653cd59f07e4a7
setal1te <149357036+cosholyknight@users.noreply.github.com>
2023-12-13 21:25:21 +0700

    done

---
+++ Prob202.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set <int> set;
+        while (n != 1) {
+            if (set.count(n)) return false;
+            set.insert(n);
+            int sum = 0;
+            while (n > 0) {
+                sum += (n % 10) * (n % 10);
+                n /= 10;
+            }
+            n = sum;
+        }
+        return true;
+    }
+};
