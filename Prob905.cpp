3ecde3c0f648e6c5ed7d19c2c26ff15c93c8f5db
setal1te <149357036+cosholyknight@users.noreply.github.com>
2023-12-25 23:51:20 +0700

    done

---
+++ Prob905.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int pos = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] % 2 == 0) {
+                swap(nums[pos], nums[i]);
+                pos++; 
+            }
+        }
+        return nums;
+    }
+};
